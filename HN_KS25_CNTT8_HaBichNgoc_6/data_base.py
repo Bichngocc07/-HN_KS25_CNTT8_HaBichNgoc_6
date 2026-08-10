3732b87bde7350800876570d99bc5d661e508643
Hà Bích Ngọc <ngoc01072006@gmail.com>
2026-08-10 11:14:03 +0700

    Add files via upload

---
+++ HN_KS25_CNTT8_HaBichNgoc_6/data_base.py
@@ -0,0 +1,7 @@
+raw_phones = [ 
+{"phone_code": "P301", "brand": "apple", "price": 25000000, "status": "available"}, 
+{"phone_code": " p101 ", "brand": "samsung", "price": 12000000, "status": "available"}, 
+{"phone_code": "P202", "brand": "xiaomi", "price": 8000000, "status": "sold"}, 
+{"phone_code": "P102", "brand": "samsung", "price": 9500000, "status": "reserved"}, 
+{"phone_code": "P302", "brand": "apple", "price": 28000000, "status": "available"} 
+]
---
+++ HN_KS25_CNTT8_HaBichNgoc_6/response.py
@@ -0,0 +1,16 @@
+{ 
+"statusCode": 200, 
+"error": null, 
+"message": "Lấy danh sách điện thoại thành công", 
+"data": [ 
+{ 
+"id": 1, 
+"phone_code": "P301", 
+"price": 25000000.0, 
+"brand": { 
+"id": 1, 
+"name": "apple" 
+} 
+} 
+] 
+} 
